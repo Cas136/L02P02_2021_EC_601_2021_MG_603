31a1771f12c0a0f9875abedf3931205cef468334
Estefanía Cristina Martínez <Tefymartinez152003@gmail.com>
2025-04-03 21:33:30 -0600

    Update comentarios_libros.cs

--- L02P02_2021_EC_601_2021_MG_603/Models/comentarios_libros.cs
+++ L02P02_2021_EC_601_2021_MG_603/Models/comentarios_libros.cs
@@ -1,13 +1,19 @@
-﻿namespace L02P02_2021_EC_601_2021_MG_603.Models
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace L02P02_2021_EC_601_2021_MG_603.Models
 {
     public class comentarios_libros
     {
+        [Key]
         public int id { get; set; }
-        public int id_libro { get; set; } // Nombre exacto como en la BD
-        public string comentarios { get; set; } // varchar(max) en BD
-        public string usuario { get; set; }
-        public DateTime created_at { get; set; }
 
-        public libros Libros { get; set; } // Para la relación
+        [Required]
+        public string comentario { get; set; }
+
+        [ForeignKey("libros")]
+        public int libro_id { get; set; }
+
+        public libros? libro { get; set; } 
     }
 }
