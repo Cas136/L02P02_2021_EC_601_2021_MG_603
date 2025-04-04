0542b42d78a966c90ec911e770b5bf490de3c0ac
Estefanía Cristina Martínez <Tefymartinez152003@gmail.com>
2025-04-03 21:32:48 -0600

    Update libros.cs

--- L02P02_2021_EC_601_2021_MG_603/Models/libros.cs
+++ L02P02_2021_EC_601_2021_MG_603/Models/libros.cs
@@ -1,16 +1,19 @@
-﻿namespace L02P02_2021_EC_601_2021_MG_603.Models
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace L02P02_2021_EC_601_2021_MG_603.Models
 {
     public class libros
     {
+        [Key]
         public int id { get; set; }
-        public string nombre { get; set; }
-        public string descripcion { get; set; }
-        public string url_imagen { get; set; }
-        public int id_autor { get; set; } // Nombre exacto como en la BD
-        public int id_categoria { get; set; }
-        public decimal precio { get; set; }
-        public char estado { get; set; }
 
-        public autores Autor { get; set; } // Para la relación
+        [Required]
+        public string titulo { get; set; }
+
+        [ForeignKey("autores")]
+        public int autor_id { get; set; }
+        
+        public autores? autor { get; set; } 
     }
 }
